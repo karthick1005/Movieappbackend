6fcb3265f45922e420f87e7647270c0f6fd8aefe
karthick <udhayakarthick.mnm4@gmail.com>
2024-09-24 12:50:58 +0530

    update on cors

---
+++ src/main/java/com/karthick/Movieapp/WebConfig.java
@@ -0,0 +1,17 @@
+package com.karthick.Movieapp;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Adjust the URL pattern based on your API
+                .allowedOrigins("http://localhost:3000") // Allow requests from React (localhost:3000)
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Define allowed HTTP methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true); // Allow credentials (optional)
+    }
+}
