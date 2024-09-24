bfb679227896910044bdf148044fd6721b130b79
karthick <udhayakarthick.mnm4@gmail.com>
2024-09-24 14:00:42 +0530

    update

--- src/main/java/com/karthick/Movieapp/WebConfig.java
+++ src/main/java/com/karthick/Movieapp/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Adjust the URL pattern based on your API
-                .allowedOrigins("http://localhost:3000") // Allow requests from React (localhost:3000)
+                .allowedOrigins("http://localhost:3000","https://movieapp-sooty-ten.vercel.app/") // Allow requests from React (localhost:3000)
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Define allowed HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials (optional)
